a66c9d722a69e2fcc1ebce009bcdb13777adfec0
Khoa Tran <Khoa Tran>
2022-04-15 18:36:35 +0700

    Update student email regex

--- apps/api/src/utils/regex/fu-email-regex.ts
+++ apps/api/src/utils/regex/fu-email-regex.ts
@@ -1,3 +1,3 @@
 export const fuEmailRegex = /^\w+@fpt\.edu\.vn$/g;
 
-export const fuStudentEmailRegex = /^\w+(\w{2}\d{6})@fpt\.edu\.vn$/gi
+export const fuStudentEmailRegex = /^\w+([a-zA-Z]{2}\d{6})@fpt\.edu\.vn$/gi
